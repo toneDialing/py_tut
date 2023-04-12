a253c006df75e1ad9afbc7c7a7628fb1ece73e6b
Matthew Rathkey <rathkeym@gmail.com>
2023-04-11 23:03:50 -0600

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+# Adds a value to a new key "Hermione"
+# Remember [] refer to keys, so Python either locates the "Hermione" key
+# in 'houses' or creates a new one.
+houses["Hermione"] = "Gryffindor"
+
+print(houses["Harry"])
+print(houses["Hermione"])
